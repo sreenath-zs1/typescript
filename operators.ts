d2e6029026bd4ed76d42b40dd821e5f27d2fcfbc
sreenathzs <82632135+sreenathzs@users.noreply.github.com>
2021-10-18 16:41:06 +0530

    arithmetic operators

---
+++ operators.ts
@@ -0,0 +1,16 @@
+var num1:number = 15
+var num2:number = 10
+var res:number = 0
+
+res = num1 + num2
+console.log("Sum: "+res);
+
+res = num1 - num2
+console.log("Differnce: "+res);
+
+res = num1 * num2
+console.log("Product: "+res);
+
+# by using arithmetic operators
+# tsc operators.ts --> After compile we need to loop the javascript script by using below command
+# node operators.js
